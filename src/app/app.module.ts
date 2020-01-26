2aaf24fb67ef7229e3bc8e5ae7706e65a8fc5260
gtagliente <gianluca.profile@gmail.com>
2020-01-26 16:17:54 +0100

    about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Navbar/navbar.component';
 import { SliderComponent } from './Slider/slider.component';
+import { AboutComponent} from './About/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SliderComponent
+    SliderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
